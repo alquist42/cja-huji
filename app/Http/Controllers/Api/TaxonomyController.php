c889b54c3f118c016fc1de589eee1aac7396360b
Sofya <uspenskayasofyasergeevna@gmail.com>
2019-11-20 12:34:23 +0200

    small refactor var names

--- app/Http/Controllers/Api/TaxonomyController.php
+++ app/Http/Controllers/Api/TaxonomyController.php
@@ -25,37 +25,35 @@ class TaxonomyController extends Controller
         DB::enableQueryLog();
 
         $model = $this->nameSpace . ucfirst(str_singular($type));
-   //     var_dump($model);
         if (!class_exists($model)) {
             return response()->json([ 'error' => 400, 'message' => 'Missing or unsupported type' ], 400);
         }
         if(request()->get('as_tree')){
-            $items = $model::where('id', '!=', '-1')
+            $sets = $model::where('id', '!=', '-1')
                 ->whereHas('sets', function($q) {
                     $q->join('projects', 'sets.id', '=', 'projects.taggable_id');
                 })
                 ->get();
 
-            $first_names = array_column($items->toArray(),'parent_id', 'id');
-//            $first_names =array_flip($first_names);
+            $found_sets = array_column($sets->toArray(),'parent_id', 'id');
             $missed = [];
-            foreach($first_names as $k=>$v){
-                if(!isset($first_names[$v]) && !in_array($v,$missed)){
+            foreach($found_sets as $k=>$v){
+                if(!isset($found_sets[$v]) && !in_array($v,$missed)){
                     $missed[]= $v;
                 }
             }
             if(!empty($missed)){
-                $AddedItems = $model::whereIn('id',$missed)->get();
+                $missedSets = $model::whereIn('id',$missed)->get();
             }
-            $items = $items->merge($AddedItems)->sortBy('id');
-            $items = $items->toTree();
+            $sets = $sets->merge($missedSets)->sortBy('id');
+            $sets = $sets->toTree();
 
         } else {
-            $items =  $model::paginate();
+            $sets =  $model::paginate();
         }
 
     //    dd(DB::getQueryLog());
-        return response()->json($items);
+        return response()->json($sets);
     }
 
     /**
