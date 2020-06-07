711f80a08d4131c8405cc9a4938762b41c5d624b
Sofya <uspenskayasofyasergeevna@gmail.com>
2020-06-07 11:42:26 +0300

    removedempty fields from image-details

--- resources/views/item/image-details.blade.php
+++ resources/views/item/image-details.blade.php
@@ -1,15 +1,23 @@
 <dl class="row my-1">
-    <dt class="col-sm-3">Copyright</dt>
-    <dd class="col-sm-9">{{ array_get($item->images,'0.copyright.name') }}</dd>
+    @if(array_get($item->images,'0.copyright.name'))
+        <dt class="col-sm-3">Copyright</dt>
+        <dd class="col-sm-9">{{ array_get($item->images,'0.copyright.name') }}</dd>
+    @endif
 
-    <dt class="col-sm-3">Photographer</dt>
-    <dd class="col-sm-9">
-        <a href="/{{ request()->project }}/browse/photographers/{{ array_get($item->images,'0.photographer.id') }}"> {{ array_get($item->images,'0.photographer.name') }}</a>
-    </dd>
+    @if(array_get($item->images,'0.photographer.id'))
+        <dt class="col-sm-3">Photographer</dt>
+        <dd class="col-sm-9">
+            <a href="/{{ request()->project }}/browse/photographers/{{ array_get($item->images,'0.photographer.id') }}"> {{ array_get($item->images,'0.photographer.name') }}</a>
+        </dd>
+    @endif
 
-    <dt class="col-sm-3">Date</dt>
-    <dd class="col-sm-9">{{ array_get($item->images,'0.date') }}</dd>
+    @if(array_get($item->images,'0.date'))
+        <dt class="col-sm-3">Date</dt>
+        <dd class="col-sm-9">{{ array_get($item->images,'0.date') }}</dd>
+    @endif
 
-    <dt class="col-sm-3">Negative / Photo. No.</dt>
-    <dd class="col-sm-9">{{ array_get($item->images,'0.negative') }}</dd>
+    @if(array_get($item->images,'0.negative'))
+        <dt class="col-sm-3">Negative / Photo. No.</dt>
+        <dd class="col-sm-9">{{ array_get($item->images,'0.negative') }}</dd>
+    @endif
 </dl>
