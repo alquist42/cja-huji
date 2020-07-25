8c4ffcfc01909b7bd71a3564587455cd1a25e07c
Pavel Mironchik <mironchikpavel@gmail.com>
2020-07-25 23:22:10 +0600

    Fix inability to attach images by selecting a folder.

--- admin/src/mixins/NestedFiles.vue
+++ admin/src/mixins/NestedFiles.vue
@@ -7,13 +7,15 @@
         for (let i = 0; i < filesAndFolders.length; i++) {
           if (filesAndFolders[i].type === 'folder') {
             try {
-              const { data } = await this.$http.post('/staff/media/get-files', {
+              const { data } = await this.$http.post('staff/media/get-files', {
                 path: filesAndFolders[i].storage_path,
+              }, {
+                baseURL: '/',
               })
 
               files = files.concat(await this.getAllNestedFiles(data.files.items.data))
             } catch (e) {
-              this.showSnackbarError()
+              this.$emit('error')
               throw e
             }
           } else {
